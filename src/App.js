d5674f8b44052dd4990e7509ae2105bce10afac4
kangsikWoo <wks09254930@gmail.com>
2023-08-24 15:34:32 +0900

    리스트 구현하기

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TodoList from "./components/todo-list/todo-list.jsx";
+
+function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
