04f4b4207f59788434d4b9358f9fdd6e2fe31c37
kangsikWoo <wks09254930@gmail.com>
2023-08-25 17:22:39 +0900

    CSS 헤더 부분

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 export default function Header({ filters, filter, onFilterChange }) {
   return (
-    <header>
-      <ul>
+    <header className={styles.header}>
+      <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
-            <button onClick={() => onFilterChange(value)}>{value}</button>
+            <button
+              // Header.module.css파일에 있는 filter의 정보와 selected의 정보를 가져옴
+              className={`${styles.filter} ${
+                filter === value && styles.selected
+              }`}
+              onClick={() => onFilterChange(value)}
+            >
+              {value}
+            </button>
           </li>
         ))}
       </ul>
